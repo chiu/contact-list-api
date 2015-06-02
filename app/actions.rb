8b579bc6b0e38549bb148d8da1c1ee053758954f
chiu <chiucode@gmail.com>
2015-06-02 22:04:01 +0000

    broken gui

--- app/actions.rb
+++ app/actions.rb
@@ -1,18 +1,11 @@
-# Homepage (Root path)
-# get '/' do
-
-# erb :index
-# end
-
-# get '/' do
-#   Contact.search_all('Rex').to_json
-# end
-
 
 get '/contacts/' do
   Contact.all.to_json
 end
 
+get '/contacts' do
+  Contact.all.to_json
+end
 
 get '/contacts/:id' do
   contact = Contact.find(params[:id])
@@ -26,13 +19,11 @@ get '/contacts/firstname/:firstname' do
   contact.to_json
 end
 
-
 get '/contacts/lastname/:lastname' do
   contact = Contact.find_all_by_lastname(params[:lastname])
    return status 404 if contact.nil?
   contact.to_json
 end
-
 
 get '/contacts/email/:email' do
   contact = Contact.find_all_by_email(params[:email])
@@ -45,8 +36,12 @@ delete '/contacts/:id/' do
   return status 404 if contact.nil?
   contact.to_json
   contact.destroy
-
-
 end
 
-# set :root, '/contacts/'
+
+
+
+get '/gui/contacts/' do
+  Contact.all.to_json
+  erb :'contacts/index'
+end
