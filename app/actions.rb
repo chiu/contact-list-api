aa574d0406b5300a1d1d302a4d3f7f8c45d2282c
chiu <chiucode@gmail.com>
2015-06-02 19:02:19 +0000

    good

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,76 @@
 # Homepage (Root path)
 get '/' do
-  erb :index
+  # require_relative 'table_manager'
+  TableManager.open_table
+  john_attributes = {firstname: 'John', lastname: 'Smith', email: 'johnsmith@gmail.com'}
+  john = Contact.new(john_attributes)
+  john.save
+  tyler_attributes = {firstname: 'Tyler', lastname: 'Smith', email: 'tylersmith@gmail.com'}
+  tyler = Contact.new(tyler_attributes)
+  tyler.save
+  john_attributes = {firstname: 'John', lastname: 'Smith', email: 'johnsmith@gmail.com'}
+  john = Contact.new(john_attributes)
+  john.save
+  tyler_attributes = {firstname: 'Tyler', lastname: 'Smith', email: 'tylersmith@gmail.com'}
+  tyler = Contact.new(tyler_attributes)
+  tyler.save
+  john_attributes = {firstname: 'John', lastname: 'Smith', email: 'johnsmith@gmail.com'}
+  john = Contact.new(john_attributes)
+  john.save
+  tyler_attributes = {firstname: 'Tyler', lastname: 'Smith', email: 'tylersmith@gmail.com'}
+  tyler = Contact.new(tyler_attributes)
+#puts tyler.inspect
+tyler.save
+
+tyler_attributes = {firstname: 'Tyler', lastname: 'Rex', email: 'tylersmith@gmail.com'}
+tyler = Contact.new(tyler_attributes)
+puts tyler.inspect
+tyler.save
+
+puts "meta test"
+# Contact.metaprogram
+puts "find by last name"
+puts Contact.find_all_by_lastname('Smith').class
+
+phone_attributes = {number_of_phone: '4324324324', contact_id: '1'}
+someone_number = Phone.new(phone_attributes)
+someone_number.save
+
+
+def generate_phone_number
+  rand.to_s[2..11] 
+
 end
+
+def generate_random_name
+
+  random_name =  (0...10).map { ('a'..'z').to_a[rand(26)] }.join
+
+end
+
+
+for i in 0..10
+
+  tyler_attributes = {firstname: generate_random_name, lastname: generate_random_name, email: 'tylersmith@gmail.com'}
+  tyler = Contact.new(tyler_attributes)
+  puts tyler.inspect
+  tyler.save
+
+end
+
+for i in 0..Contact.count-1
+
+  phone_attributes = {number_of_phone: generate_phone_number , contact_id: rand(Contact.count)}
+  someone_number = Phone.new(phone_attributes)
+  someone_number.save
+
+
+end
+
+
+erb :index
+end
+
+
+
+
--- app/models/table_manager.rb
+++ app/models/table_manager.rb
@@ -10,62 +10,42 @@ class TableManager
 
 
   def self.open_table
-
-# Output messages from AR to STDOUT
-ActiveRecord::Base.logger = Logger.new(STDOUT)
-
-puts "Establishing connection to database ..."
-
-
-# heroku-postgres-3780b633 :: amber
-ActiveRecord::Base.establish_connection(
-  adapter: 'postgresql',
-  encoding: 'unicode',
-  pool: 5,
-  database: 'demv8hbqtccc8t',
-  username: 'rjyqxzxnjhxfgn',
-  password: 'Lx6LUJpdJriPW_Pi0JIWqlp4FA',
-  host: 'ec2-54-243-187-196.compute-1.amazonaws.com',
-  port: 5432,
-  min_messages: 'error'
-  )
-
-puts "CONNECTED"
-
-puts "Setting up Database (recreating tables) ..."
-
-ActiveRecord::Schema.define do
-
-  drop_table :contacts if ActiveRecord::Base.connection.table_exists?(:contacts)
-  drop_table :phones if ActiveRecord::Base.connection.table_exists?(:phones)
-  # drop_table :phone_table if ActiveRecord::Base.connection.table_exists?(:phone_table)
-
-  create_table :contacts do |t|
-    t.column :firstname, :string
-    t.column :lastname, :string
-    t.column :email, :string
-    t.timestamps
-  end
-
-  create_table :phones do |t|
-    t.references :contact
-    t.column :number_of_phone, :string
-    # t.column :owner_id, :integer
-    # t.column :email, :string
-    t.timestamps
+    ActiveRecord::Base.logger = Logger.new(STDOUT)
+    puts "Establishing connection to database ..."
+    ActiveRecord::Base.establish_connection(
+      adapter: 'postgresql',
+      encoding: 'unicode',
+      pool: 5,
+      database: 'demv8hbqtccc8t',
+      username: 'rjyqxzxnjhxfgn',
+      password: 'Lx6LUJpdJriPW_Pi0JIWqlp4FA',
+      host: 'ec2-54-243-187-196.compute-1.amazonaws.com',
+      port: 5432,
+      min_messages: 'error'
+      )
+    puts "CONNECTED"
+    puts "Setting up Database (recreating tables) ..."
   end
 
 
+  def self.redo_table
+    ActiveRecord::Schema.define do
+      drop_table :contacts if ActiveRecord::Base.connection.table_exists?(:contacts)
+      drop_table :phones if ActiveRecord::Base.connection.table_exists?(:phones)
 
+      create_table :contacts do |t|
+        t.column :firstname, :string
+        t.column :lastname, :string
+        t.column :email, :string
+        t.timestamps
+      end
 
-
-
-
-end
-
-puts "Setup DONE"
-
-end
-
-
+      create_table :phones do |t|
+        t.references :contact
+        t.column :number_of_phone, :string
+        t.timestamps
+      end
+    end
+    puts "Setup DONE"
+  end
 end
