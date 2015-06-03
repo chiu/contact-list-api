49211d55d9916555c78c47aa65b627e316c1b7ad
chiu <chiucode@gmail.com>
2015-06-03 01:55:03 +0000

    adding show all users

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -19,102 +19,38 @@ $(document).ready(function(){
      $("#last_round_random").text(randomNumber.toString());
    }
 
+   
 
-   function betMoney() {
-    console.log("bet start")
-    if (!userMoney){
-      userMoney = MONEY_INITIAL;
-      $("#user_money_left").text(userMoney.toString());
-    }
-    if (!betAmount ){
-      betAmount = BET_MINIMUM;
-      $("#bet_feedback").text("Set to default of: " + betAmount.toString());
-    }
-    if (!userGuess ){
-      userGuess = GUESS_MINIMUM;
-      $("#guess_feedback").text("Set to default of: " + userGuess.toString());
-    }
-    console.log("userMoney:" + userMoney + typeof(userMoney));
-    console.log("betAmount:" + betAmount + typeof(betAmount));
-    console.log("userguess:" + userGuess + typeof(userGuess));
+   function showAllContacts(){
 
-    generateRandomNumber();
+     $.getJSON("/contacts/", function( monkey ){
 
-    if (userGuess === randomNumber){
-      console.log("You won!");
-      $("#last_round_result").text("You won the bet.");
-      userMoney += parseInt(betAmount) * 2;
-    } else if(userGuess === randomNumber + 1 || userGuess === randomNumber -1){
-      $("#last_round_result").text("You were off by 1: keep your bet");
-      console.log("off by one. ")
-      userMoney = userMoney 
-    } else {
-      $("#last_round_result").text("You lost.");
-      console.log("lost")
-      userMoney = userMoney - betAmount;
-    }
-    localStorage.setItem('userMoney', userMoney);
-    $("#user_money_left").text(userMoney.toString());
-    makeStockpile();
-  }
+      console.log(JSON.stringify(monkey));
+      ar items = [];
+      $.each( monkey, function( key, val ) {
+        items.push( "<li id='" + key + "'>" + val + "</li>" );
+      });
+
+      $( "<ul/>", {
+        "class": "my-new-list",
+        html: items.join( "" )
+      }).appendTo( "body" );
 
 
-  var makeStockpile = function(){
-    $('#stockpile div').remove();
-    for(i=0;i< Math.floor(userMoney/10); i++ ){
-     $('#stockpile').append("<div>cash</div>");
+    });
+
+
+
    }
- }
+
+   
+   $("#show_all_contacts").on('click', showAllContacts)
 
 
- function resetMoney() {
-  userMoney = MONEY_INITIAL;
-  $("#user_money_left").text(userMoney.toString());
-  makeStockpile();
-}
-
-function updateBetAmount(){
-  console.log("inside bet amount")
-  betAmount = +$("#betAmount").val();
-  console.log(betAmount, BET_MINIMUM);
-  if(betAmount < BET_MINIMUM ){
-    betAmount = BET_MINIMUM;
-    $("#betAmount").val(BET_MINIMUM);
-    $("#bet_feedback").text("You under the minimum, set to default of " + BET_MINIMUM);
-  } else if (betAmount > BET_MAXIMUM) {
-    betAmount = BET_MAXIMUM;
-    $("#betAmount").val(BET_MAXIMUM);
-    $("#bet_feedback").text("You are over the maximum bet, set to maximum of " +BET_MAXIMUM)
-  } else if (betAmount !== null ) {
-    $("#bet_feedback").text("You bet the following amount: " + betAmount + "! How are you today?");
-  }
-  console.log("Bet amount:" + betAmount);
-}
-
-function updateGuess(){
-  userGuess = +$("#userGuess").val();
-
-
-  console.log("guess value, type: " + userGuess + typeof(userGuess));
-  console.log(userGuess, GUESS_MINIMUM);
-  if(userGuess < GUESS_MINIMUM) {
-    userGuess = GUESS_MINIMUM; 
-    $("#userGuess").val(GUESS_MINIMUM);
-    $("#guess_feedback").text("You are under the minimum, set to minimum of " + GUESS_MINIMUM)
-  } else if (userGuess > GUESS_MAXIMUM ) {
-    userGuess = GUESS_MAXIMUM;
-    $("#userGuess").val(GUESS_MAXIMUM);
-    $("#guess_feedback").text("You are over the maximum, set to maximum of " +GUESS_MAXIMUM)
-  } else if (userGuess !== null) {
-    $("#guess_feedback").text("You guessed the following number: " + userGuess);
-  }
-  console.log("guess amount:" + userGuess + typeof(userGuess));
-}
-
-$("#betButton").click(betMoney);
-$("#resetMoney").click(resetMoney);
-$("#betAmount").on('change', updateBetAmount);
-$("#userGuess").on('change', updateGuess);
+// $("#betButton").click(betMoney);
+// $("#resetMoney").click(resetMoney);
+// $("#betAmount").on('change', updateBetAmount);
+// $("#userGuess").on('change', updateGuess);
 
 })();
 });
